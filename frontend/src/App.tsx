5e3cea291dc278272481918985253f84c7d5fd91
Rhuan S. Hianc <rhuanhianc123@gmail.com>
2022-03-26 20:06:01 -0300

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-
+import {ReactComponent as GitHubSvg} from './assets/gitSvg.svg';
 function App() {
   return (
-    <h1>testando</h1>
+    <header>
+      <nav>
+        <div>
+          <h1>DSMovie</h1>
+          <a href="https://github.com/rhuanhianc">
+          <div>
+            <GitHubSvg/>
+            <p> /rhuanhianc  </p>
+          </div>
+          </a>
+        </div>
+      </nav>
+    </header>
   );
 }
 
