8fcf744500a86d46e4bd3ecd428342bfb1d2c389
Rhuan S. Hianc <rhuanhianc123@gmail.com>
2022-04-02 13:38:41 -0300

    moviestars

---
+++ frontend/src/components/moviestars/index.tsx
@@ -0,0 +1,19 @@
+import {ReactComponent as StarFull} from 'assets/star-full.svg';
+import {ReactComponent as StarHalf} from 'assets/star-half.svg';
+import {ReactComponent as StarEmpty} from 'assets/star-empty.svg';
+import "./styles.css";
+
+function MovieStars() {
+
+    return (
+        <div className="dsmovie-stars-container">
+            <StarFull />
+            <StarFull />
+            <StarFull />
+            <StarHalf />
+            <StarEmpty />
+        </div>
+    );
+
+}
+export default MovieStars;
